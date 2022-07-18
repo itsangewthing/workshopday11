06bc24902af079123f41d8207acdee8cc5a3040b
partytaitai <partytaitai@MacBook-Pro-3.local>
2022-07-18 16:38:12 +0800

    sys prop

--- src/main/java/sg/edu/nus/iss/d11workshop/D11workshopApplication.java
+++ src/main/java/sg/edu/nus/iss/d11workshop/D11workshopApplication.java
@@ -1,5 +1,11 @@
 package sg.edu.nus.iss.d11workshop;
 
+import java.util.Collection;
+import java.util.Collections;
+
+import org.springframework.beans.propertyeditors.StringArrayPropertyEditor;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.DefaultApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,8 +13,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class D11workshopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(D11workshopApplication.class, args);
-	}
+		// SpringApplication.run(D11workshopApplication.class, args);
+		SpringApplication app = new SpringApplication(D11workshopApplication.class);
+
+		String port ="8085";
+
+		ApplicationArguments cli0pts = new DefaultApplicationArguments(args);
+		if (cli0pts.containsOption("port")){
+			port = cli0pts.getOptionValues("port").get(0);
+
+		app.setDefaultProperties(Collections.singletonMap("server port",port));
+		System.out.printf("application started on port %s\n", port);
+		app.run(args);	
+
+		}
+
+		}
 
 		// private String port = 
 
